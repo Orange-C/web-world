80d99ac906e61ae3f7e8ec977d376ea100fd67c5
Orange-C <741434157@qq.com>
2017-02-13 15:55:44 +0800

    add back && ui update

--- src/js/index.js
+++ src/js/index.js
@@ -3,15 +3,27 @@ import init from './init'
 
 let singleBtn = document.querySelector('.single-btn');
 let doubleBtn = document.querySelector('.double-btn');
+let resetBtn = document.querySelector('.reset-btn');
+let backBtn = document.querySelector('.back-btn');
+let singleInfo = document.querySelector('.single-info');
+let doubleInfo = document.querySelector('.double-info');
+
+backBtn.addEventListener('click', (e) => {
+    window.location.reload();
+})
 
 singleBtn.addEventListener('click', (e) => {
-    document.querySelector('.single-info').style.display = 'block';
-    document.querySelector('.double-info').style.display = 'none';
+    resetBtn.style.display = 'block';
+    backBtn.style.display = 'block';
+    singleInfo.style.display = 'block';
+    doubleInfo.style.display = 'none';
     init('single');
 });
 
 doubleBtn.addEventListener('click', (e) => {
-    document.querySelector('.single-info').style.display = 'none';
-    document.querySelector('.double-info').style.display = 'block';
+    resetBtn.style.display = 'block';
+    backBtn.style.display = 'block';
+    singleInfo.style.display = 'none';
+    doubleInfo.style.display = 'block';
     init('double');
 });
