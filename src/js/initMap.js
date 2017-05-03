bdcececfb7e018fdea8f20b330c0b65f9778d42a
orange <741434157@qq.com>
2017-05-03 21:00:42 +0800

    whatever random

--- src/js/initMap.js
+++ src/js/initMap.js
@@ -3,21 +3,43 @@ import { createNormalBlock, createDoor, createBowl, createStairs, createGoal, cr
 import { collisionObjs } from './collision';
 import { speedUp, speedDown, jumpHigh, calcOffset, initName } from './utils';
 
-export function initMap(dom) {
-    if(dom) {
-        initBase(dom, 72, 12, 2);
-    } else {
-        var plane = new THREE.Mesh(new THREE.PlaneGeometry(16*config.focalDistance, 16*config.focalDistance),
-            new THREE.MeshLambertMaterial({
-                map: config.texture.ground,
-                // color: 0xcccccc
-            })
-        );
-        plane.rotateX(-Math.PI/2);
-        plane.receiveShadow = true;
-        config.plane.push(plane);
-        config.scene.add(plane);
+var objStyle = {
+    div: 'stone',
+    a: 'gold',
+    ul: 'stone',
+    li: 'stone',
+    input: 'blue',
+    img: 'red',
+    form: 'stone'
+};
+var objPos = [
+    { x: 40, z: 0},
+    { x: 40, z: -40},
+    { x: 0, z: -40},
+    { x: -40, z: -40},
+    { x: -40, z: 0},
+    { x: -40, z: 40},
+    { x: 0, z: 40},
+    { x: 40, z: 40}
+];
+var pos = 0;
+var goalArr = [];
 
+export function initMap(dom) {
+    var plane = new THREE.Mesh(new THREE.PlaneGeometry(16*config.focalDistance, 16*config.focalDistance),
+        new THREE.MeshLambertMaterial({
+            map: config.texture.ground,
+            // color: 0xcccccc
+        })
+    );
+    plane.rotateX(-Math.PI/2);
+    plane.receiveShadow = true;
+    config.plane.push(plane);
+    config.scene.add(plane);
+
+    if(dom) {
+        initWeb(dom);
+    } else {
         // 出生台
         createBowl({
             bowlW: 15,
@@ -236,165 +258,141 @@ export function initMap(dom) {
             })
         }
         config.initGoal();
-
-        // createWindow({
-        //     pos: [20, 20, 20]
-        // });
-
-        // var obj2 = createNormalBlock({
-        //     len: [8, 2, 8],
-        //     pos: [-8, 3, 0],
-        //     color: 0xf8f8f8
-        // });
-        // collisionObjs.push(obj2);
-        // config.scene.add(obj2);
-
-        // var obj3 = createNormalBlock({
-        //     len: [8, 2, 8],
-        //     pos: [-14, 5, 0],
-        //     color: 0xf8f8f8
-        // });
-        // collisionObjs.push(obj3);
-        // config.scene.add(obj3);
-
-        // var wall1 = createNormalBlock({
-        //     len: [30, 16, 2],
-        //     pos: [-10, 8, 4.8],
-        //     color: 0xffffff,
-        //     transparent: true,
-        //     shadow: false,
-        // });
-        // collisionObjs.push(wall1);
-        // config.scene.add(wall1);
-
-        // var wall2 = createNormalBlock({
-        //     len: [30, 16, 2],
-        //     pos: [-10, 8, -4.8],
-        //     color: 0xffffff,
-        //     transparent: true,
-        //     shadow: false,
-        // });
-        // collisionObjs.push(wall2);
-        // config.scene.add(wall2);
     }
 }
 
-function initBase(arr, len, dis, height, offset) {
-    let objLen = len - dis;
-    arr.forEach((v, k) => {
-        if(!(k >= 4 && height > 3)) {
-            console.log(v.name);
-            // base config
-            let objColor = 0xf8f8f8; // default
-            let objHeight = 4;
-            let objOffset = calcOffset(len, k%4);
-            let objTransparent, objShadow;
-            let heightOffset = 0;
-            let lenOffset = 0;
-            let objType = 'stone';
-            
-            // offset calc
-            if(height < 3) {
-                let planeOffset = -Math.floor(k/4) * 144;
-                if(k%4 === 0) {
-                    let plane = new THREE.Mesh(new THREE.PlaneGeometry(18*config.focalDistance, 18*config.focalDistance),
-                        new THREE.MeshLambertMaterial({
-                            map: config.texture.ground,
-                        })
-                    );
-                    plane.rotateX(-Math.PI/2);
-                    plane.position.setZ(planeOffset);
-                    plane.receiveShadow = true;
-                    config.plane.push(plane);
-                    config.scene.add(plane);
-                }
-                objOffset.offsetZ += planeOffset;
-            }
-            if(offset) {
-                objOffset.offsetX += offset.offsetX;
-                objOffset.offsetZ += offset.offsetZ;
-            }
-
-            let type = Date.now() % 2;
-
-            if(v.name === 'a') {
-                objType = 'gold';
-                objHeight = 1;
-                objTransparent = true;
-                objShadow = false;
-                lenOffset = objLen + lenOffset < 4 ? 4 : 0;
-                heightOffset = k%4 ? (k%4) * 5 + 3 : 3;
-                // objColor = 0xFC5754; // red
-            }
-
-            if(v.name === 'input') {
-                objType = type ? 'blue' : 'red';
-                objHeight = 4;
-                objTransparent = true;
-                objShadow = false;
-                lenOffset = objLen + lenOffset < 4 ? 3 : 0;
-                heightOffset = 1.2;
-            }
-
-            if(v.name === 'ul'|| v.name === 'select') {
-                lenOffset = -8;
-                objHeight = 4;
-            }
-
-            if(v.name === 'li'|| v.name === 'option') {
-                lenOffset = 4.1;
-                objHeight = 1;
-                heightOffset = -1;
-            }
-
-            if(v.name === 'option'|| v.name === 'select') {
-                objShadow = false;
-            }
-
-            if(v.name === 'span') {
-                objTransparent = true;
-                objShadow = false;
-            }
-
-            if(v.name === 'img') {
-                objHeight = 1;
-                lenOffset = 4;
-            }
-
-            // init obj
-            let obj = createNormalBlock({
-                type: objType,
-                len: [objLen + lenOffset, objHeight, objLen + lenOffset],
-                pos: [objOffset.offsetX, height + heightOffset, objOffset.offsetZ],
-                color: objColor,
-                transparent: objTransparent,
-                shadow: objShadow,
-            });
-            obj.name = initName();
-            
-            if(v.name === 'a') {
-                obj.penetrable = true;
-                obj.callback = jumpHigh;
-            }
-
-            if(v.name === 'input') {
-                obj.penetrable = true;
-                obj.callback = type ? speedUp : speedDown;
-            }
-            
-            if(!(v.name === 'option'|| v.name === 'select')) {
-                collisionObjs.push(obj);
-            }
-
-            config.scene.add(obj);
-            if(v.children.length && v.name !== 'a') {
-                let newDis = dis/2 > 4 ? dis/2 : 4;
-                initBase(v.children, objLen/2, newDis, height + 4, objOffset);
-            }
-        }
-    })
-}
 
 function initWeb(dom) {
-    
+    console.log(dom);
+    // 出生台
+    createBowl({
+        bowlW: 15,
+        sideW: 1,
+        sideH: 2,
+        pos: [0, 0, 0]
+    })
+
+    parseDom(dom, 0)
+
+    //goal
+    config.initGoal = function() {
+        goalArr.forEach((cb) => {cb()});
+    }
+    config.initGoal();
+}
+
+function parseDom(arr, height) {
+    arr.forEach((v) => {
+        console.log(v.name);
+        if(v.name === 'div') {
+            var obj = createNormalBlock({
+                type: pos%2 ? 'stone' : 'wall',
+                len: [30 - height*1.5, 2, 30 - height*1.5],
+                pos: [objPos[pos].x, height + 1, objPos[pos].z],
+            });
+            collisionObjs.push(obj);
+            config.scene.add(obj);
+        }
+
+        if(v.name === 'form') {
+            let xx = objPos[pos].x;
+            let zz = objPos[pos].z;
+            let hh = height + 9;
+            
+            createWindow({
+                pos: [xx, hh, zz]
+            });
+
+            let cb = function () {
+                createGoal({
+                    pos: [xx, hh - 1.5, zz]
+                })
+            }
+
+            goalArr.push(cb);
+        }
+
+        if(v.name === 'input') {
+            let xx = objPos[pos].x;
+            let zz = objPos[pos].z;
+            let hh = height + 4;
+
+            let cb = function () {
+                createGoal({
+                    pos: [xx, hh - 1.5, zz]
+                })
+            }
+
+            goalArr.push(cb);
+        }
+
+        if(v.name === 'ul') {
+            let conf = {
+                stairW: 8,
+                count: v.children.length || 1,
+                pos: [objPos[pos].x, height + 0.5, objPos[pos].z]
+            }
+            if(pos === 7 || pos === 0) conf.zz = -1;
+            if(pos === 1 || pos === 2) conf.xx = -1;
+            if(pos === 3 || pos === 4) conf.zz = 1;
+            if(pos === 5 || pos === 6) conf.xx = 1;
+            
+            createStairs(conf);
+        }
+
+        if(v.name === 'span') {
+            var speed1 = createNormalBlock({
+                type: 'blue',
+                len: [8, 8, 1],
+                pos: [objPos[pos].x, height + 12, objPos[pos].z],
+                transparent: true,
+                shadow: false,
+            });
+            speed1.name = initName();
+            speed1.penetrable = true;
+            speed1.callback = speedUp;
+            collisionObjs.push(speed1);
+            config.scene.add(speed1);
+        }
+
+        if(v.name === 'p') {
+            var speed1 = createNormalBlock({
+                type: 'red',
+                len: [8, 8, 1],
+                pos: [objPos[pos].x, height + 12, objPos[pos].z],
+                transparent: true,
+                shadow: false,
+            });
+            speed1.name = initName();
+            speed1.penetrable = true;
+            speed1.callback = speedDown;
+            collisionObjs.push(speed1);
+            config.scene.add(speed1);
+        }
+
+        if(v.name === 'a') {
+             var jump1 = createNormalBlock({
+                type: 'gold',
+                len: [10, 1, 10],
+                pos: [objPos[pos].x, height + 4, objPos[pos].z],
+                transparent: true,
+                shadow: false,
+            });
+            jump1.name = initName();
+            jump1.penetrable = true;
+            jump1.callback = jumpHigh;
+            collisionObjs.push(jump1);
+            config.scene.add(jump1);
+        }
+
+        pos++;
+        if(pos === 8) {
+            height = height + 5;
+            pos = 0
+        }
+
+        if(v.children.length && v.name !== 'a' && v.name !== 'ul') {
+            parseDom(v.children, height);
+        }
+    })
 }
